fe744154b15649ba79893499f12f0f26d82f4a57
Sam Samai <sam@studio2pi.com.au>
2018-07-27 18:33:02 +1000

    Compiles and runs the data migrations

---
+++ lib/migration.ex
@@ -0,0 +1,18 @@
+defmodule EctoData.Migration do
+  @doc false
+  defmacro __using__(_) do
+    quote location: :keep do
+      import EctoData.Migration
+      @disable_ddl_transaction false
+      @before_compile EctoData.Migration
+    end
+  end
+
+  @doc false
+  defmacro __before_compile__(_env) do
+    quote do
+      def __migration__,
+        do: [disable_ddl_transaction: @disable_ddl_transaction]
+    end
+  end
+end
